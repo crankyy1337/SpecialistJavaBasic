1ed7ff7435080f5c02400a6fc8b14e64ba99d52b
crankyy1337 <muradeligeorge@gmail.com>
2025-02-06 22:17:02 +0300

    lab 6_1 done

---
+++ Exception_lab_6_1/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int factorialCycle(int n) {
+
+        int factorial = 1;
+        if (n < 0) throw new IllegalArgumentException("illegal input");
+        for (int i = 1; i <= n; i++) {
+            factorial = Math.multiplyExact(factorial, i);
+        }
+        return factorial;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите число для подсчета его факториала: ");
+
+        try (Scanner sc = new Scanner(System.in)) {
+            String input = sc.nextLine();
+            int inputNumber = Integer.parseInt(input);
+            System.out.printf("Факториал числа %d = %d", inputNumber, factorialCycle(inputNumber));
+        } catch (IllegalArgumentException | ArithmeticException ex) {
+            System.err.print(ex.getMessage());
+        }
+    }
+}
